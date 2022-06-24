f77e027ae43d463830869fbc73b2cf031762010c
wachira <wachiraa954@gmail.com>
2022-06-24 13:25:31 +0300

    tasklister assg done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit',(e) => {
+  e.preventDefault()
+  makeTask(e.target['new-task-description'].value)
+  form.requestFullscreen()
+   })
+
+  })
+
+  function makeTask (task){
+    let li = document.createElement('li');
+    let button = document.createElement('button')
+    button.addEventListener('click', deleteTask)
+    button.textContent = 'Delete'
+    li.textContent = task + ""
+    li.appendChild(button)
+    document.getElementById('task').appendChild(li)
+  
+  }
+
+  function deleteTask(e){
+    e.target.parentNode.remove()
+  }
